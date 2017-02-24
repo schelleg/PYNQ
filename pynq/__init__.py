22199b0804265c97b63d1583bc74f9a149bd858b
Yun Rock Qu <yunq@xilinx.com>
2017-02-23 16:51:30 -0800

    Adding clock management modules (#3)
    * adding ps clock modules
    
    * able to get and set PL clocks
    
    * revise example notebook explanation
    
    * revise Clock() class API to use property
    
    * polish board_ps_clock example
    
    * adding metaclass for Clock to eliminate object instantiation
    
    * rename Clock -> Clocks

--- pynq/__init__.py
+++ pynq/__init__.py
@@ -40,6 +40,7 @@ from .pl import PL
 from .pl import PL_SERVER_FILE
 from .pl import Bitstream
 from .pl import Overlay
+from .ps import Clocks
 from .interrupt import Interrupt
 from .xlnk import Xlnk
 
